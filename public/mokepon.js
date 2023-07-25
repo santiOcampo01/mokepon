93006235cad86a6e6f50a810fa7586c3f3e587db
Santiago Mejia Ocampo <santimejiaok@gmail.com>
2023-07-25 15:48:17 -0500

    moke

--- public/mokepon.js
+++ public/mokepon.js
@@ -16,8 +16,8 @@ const reiniciar = document.getElementById("reinicio")           //llama a la sec
 joinGame()
 
 function joinGame() {
-    fetch("https://santiocampo01.github.io/mokepon/playmokepon/join/")
-            .then(function (res) {
+    fetch("https://santiocampo01.github.io/mokepon/join")
+        .then(function (res) {
             console.log(res)
             if (res.ok) {
                 res.text()
@@ -282,8 +282,7 @@ function mokeponSeleccionado() {
 }
 
 function mokeponselection(mokeponJugador) {
-    fetch("https://santiocampo01.github.io/mokepon/
-    /" + playerId, {
+    fetch("https://santiocampo01.github.io/mokepon/playmokepon/" + playerId, {
         method: "post",
         headers: {
             "Content-Type": "application/json"
@@ -376,8 +375,7 @@ function Atacar(ataquesMokepones) {
  
 
  function sendAttacks () {
-    fetch("https://santiocampo01.github.io/mokepon/
-    /" + playerId + "/ataques", {
+    fetch("https://santiocampo01.github.io/mokepon/playmokepon/" + playerId + "/ataques", {
         method: "post",
         headers:{
             "Content-Type": "application/json"
@@ -389,8 +387,7 @@ function Atacar(ataquesMokepones) {
     intervalo = setInterval(getAttacks, 1000)
  }
  function getAttacks() {
-    fetch("https://santiocampo01.github.io/mokepon/
-    /" + enemyId + "/ataques")
+    fetch("https://santiocampo01.github.io/mokepon/playmokepon/" + enemyId + "/ataques")
         .then(function (res) {
             if (res.ok) {
                 res.json()
@@ -543,8 +540,7 @@ function drawMap() {
 }
 
 function sendposition(x, y) {
-    fetch(`https://santiocampo01.github.io/mokepon/
-    /${playerId}/position`, {
+    fetch(`https://santiocampo01.github.io/mokepon/playmokepon/${playerId}/position`, {
         method: "post",
         headers:  {
             "Content-Type": "application/json"
